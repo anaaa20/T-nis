2e10db300fb0698cbb90b8f80b226bde8945c709
anaaa20 <00001116012017sp@al.educacao.sp.gov.br>
2024-11-27 20:34:04 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,29 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Esporte',
+    'Quantos sets são necessários para vencer uma partida de tênis em torneios masculinos de Grand Slam?',
+    'Em torneios masculinos de Grand Slam, são necessários 3 sets para vencer uma partida (melhor de 5 sets).'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Esporte',
+    'Qual é o nome do principal torneio de tênis realizado na grama?',
+    'O principal torneio de tênis realizado na grama é Wimbledon.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Esporte',
+    'O que significa a pontuação "love" no tênis?',
+    'No tênis, "love" significa zero pontos.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Esporte',
+    'Qual é a superfície utilizada no torneio Roland Garros?',
+    'O torneio Roland Garros é jogado em quadras de saibro.'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem é considerado o maior vencedor de Grand Slams no tênis até 2024?',
+    'O maior vencedor de Grand Slams até 2024 é Novak Djokovic, com 24 títulos.'
 )
